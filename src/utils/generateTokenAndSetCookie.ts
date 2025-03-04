08d8b25270c45698752687f82e09af54d64a9211
SaumayGoel <sg0627@srmist.edu.in>
2025-03-05 01:18:34 +0530

    fix

--- src/utils/generateTokenAndSetCookie.ts
+++ src/utils/generateTokenAndSetCookie.ts
@@ -5,14 +5,19 @@ export const generateTokenAndSetCookie = (res: Response, userId: string) => {
   if (!process.env.JWT_SECRET) {
     throw new Error("JWT_SECRET is not defined");
   }
+
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7D",
   });
 
+  const isProduction = process.env.NODE_ENV === "production";
+
   res.cookie("token", token, {
     httpOnly: true,
-    secure: true,
-    sameSite: "strict",
+    secure: isProduction,
+    sameSite: isProduction ? "lax" : "strict",
+    domain: isProduction ? ".acadmate.in" : undefined,
+    path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
